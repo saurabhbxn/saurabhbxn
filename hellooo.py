ba2ae664963ed3fa7854a63a1e572515f6baa9b1
saurabhbxn <bayanaboy141@gmail.com>
2025-10-17 11:12:20 +0530

    helloo.py

---
+++ hellooo.py
@@ -0,0 +1,29 @@
+ print ( "hello' worl\".")
+
+
+
+# Escape Sequence Description Example Output
+# \n New line     Hello\nworld Hello
+# World 
+# \t     Horizontal tab Hello\world world
+# \\    Backslash       \\    \
+# \r    Carrige  return Helloo\b     world
+#  Backspace    Helloo\    Hello
+#  \f    form feed  Rarely  used
+#  \v    Form feed  Rarely  used
+#  \a    Bell  (alert)    May beep on some systems
+#  \'    Single   quote        \'   '
+#  \"    Double   quote        \"   "
+
+
+
+# print ("hello   this  is\n  line or newline.  \n")
+# print ("hello   this  is\b  backspace.  \n")
+# print ("hello   this  is\a alert or bell.  \n")
+# print ("hello   this  is\f  form feed.  \n")
+# print ("hello   this  is\v  vertical  tab.  \n")
+# print ("hello   this  is\r  carriage return.  \n")
+# print ("hello   this  is\t  horizontal tab.   \n")
+# print ("hello   this  is\\  backslash.  \n")
+# print ("hello   this  is\'  single quote.   \n")
+# print ("hello   this  is\"  double quote.   \n")
