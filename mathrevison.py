aaf43bfaed4443fa3ec45c12a8a5fdd9c4598539
saurabhbxn <bayanaboy141@gmail.com>
2025-10-28 13:43:31 +0530

    revison

---
+++ mathrevison.py
@@ -0,0 +1,28 @@
+Jaipur1 = 35
+Jaipur2 = 20
+sub=Jaipur1+Jaipur2
+print (sub)
+
+sub=Jaipur1-Jaipur2
+print (sub)
+
+sub=Jaipur1*Jaipur2
+print (sub)
+
+sub=Jaipur1/Jaipur2
+print (sub)
+
+sub=Jaipur1//Jaipur2
+print (sub)
+
+sub=Jaipur1%Jaipur2
+print (sub) 
+
+Btp1 = "rohit"
+Btp2 = "kumar"
+
+print (Btp1+Btp2) 
+
+print ("#"*10)
+
+print ("_"*20)
