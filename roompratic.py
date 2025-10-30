987733a81ac65848fa49f6f2434f5056896d27ea
saurabhbxn <bayanaboy141@gmail.com>
2025-10-30 10:15:32 +0530

    Add files via upload
    room 1

---
+++ roompratic.py
@@ -0,0 +1,58 @@
+sub="*"*1
+print(sub)
+
+sub="*"*2
+print(sub) 
+
+
+sub="*"*3
+print(sub) 
+
+
+sub="*"*4
+print(sub) 
+
+
+sub="*"*5
+print(sub) 
+
+
+sub="*"*6
+print(sub) 
+
+
+sub="*"*5
+print(sub) 
+
+
+sub="*"*4
+print(sub) 
+
+
+sub="*"*3
+print(sub) 
+
+
+sub="*"*2
+print(sub) 
+
+
+sub="*"*1
+print(sub) 
+
+print ("1 2 3 4 5 6 7")
+print ("1 2 3 4 5 6")
+print ("1 2 3 4 5")
+print ("1 2 3 4")
+print ("1 2 3")
+print ("1 2")
+print ("1")
+
+
+
+print ("1                     1")
+print ("1 2                 2 1")
+print ("1 2 3.            3 2 1")
+print ("1 2 3 4         4 3 2 1")
+print ("1 2 3 4 5     5 4 3 2 1")
+print ("1 2 3 4 5 6 6 5 4 3 2 1")
