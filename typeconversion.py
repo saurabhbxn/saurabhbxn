463d59b8faebd8892635e1da609282249e666a0c
saurabhbxn <bayanaboy141@gmail.com>
2025-10-31 10:28:21 +0530

    typerevison

---
+++ typeconversion.py
@@ -0,0 +1,26 @@
+str (35)
+print (35)
+type  (str (35)) 
+print (type  (str (35)))
+
+a=396
+str (a)
+print (type(a))
+
+print("*"*50)
+
+a=5.9
+a=str (a)
+print (type(a))
+print (a)
+
+
+print("*"*50) 
+
+
+
+x=True
+print(type(x))
+a=str(x)
+print(type(a))
+print(type(x))
