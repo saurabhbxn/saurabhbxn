153d8e16a6f709174cd3d58ac6e18bc4200cae20
saurabhbxn <bayanaboy141@gmail.com>
2025-11-06 07:58:45 +0530

    shop saman

---
+++ easyshop.py
@@ -0,0 +1,10 @@
+d=int(input("Enter amount : "))
+a=int(input("Enter buy milk : "))
+b=int(input("Enter buy china : "))
+c=int(input("Enter but tea : "))
+
+ts=a*25+b*50+c*20
+
+print(a+b+c)
+print(a*25+b*50+c*20)
+print(d-ts)
