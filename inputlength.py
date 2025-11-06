db80d17b8fc8c08c57f03646341d1b1748cb609c
saurabhbxn <bayanaboy141@gmail.com>
2025-11-06 07:59:36 +0530

    area

---
+++ inputlength.py
@@ -0,0 +1,11 @@
+a=input("Enter up: ")
+b=input("Enter down: ")
+
+print("Area is : ")
+print( int(a) * int(b))
+
+a = input("Enter Length: ")
+b = input("Enter Length: ")
+
+print("Area is : ")
+print( int(a) * int(b) )
