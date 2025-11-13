5b0bcb6863990513c55b13a3b11bbaeb4b386011
saurabhbxn <bayanaboy141@gmail.com>
2025-11-13 13:09:17 +0530

    new ex file

---
+++ newex.py
@@ -0,0 +1,20 @@
+numname="John"
+age=20
+print("my name is",numname)
+print("I am",age,"years.")
+
+
+
+a=input('number1 ')
+b=input('number2 ')
+
+a=int(a)
+b=int(b)
+
+print  (a+b)
+
+print  (a-b)
+
+print  (a*b)
+
+print  (a/b)
