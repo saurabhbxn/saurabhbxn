87547f654466e4be26d0bc8956d4f0ea151a0666
saurabhbxn <bayanaboy141@gmail.com>
2025-12-01 11:46:16 +0530

    loopfile

---
+++ loopfile.py
@@ -0,0 +1,20 @@
+# i=5
+# while i:
+#     print(i)
+#     i=i-1
+
+# i=1
+# while i<=100:
+#     print("hello world")
+#     i+=1
+
+# i=2
+# while i<=50:
+#     print(i)
+#     i+=2
+
+# i=int(input("number"))
+# a=1
+# while a<=i:
+#     print(a)
+#     a+=1
