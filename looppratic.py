991683fcd7bd82c53e8ff07f2b3dc92a140d656a
saurabhbxn <bayanaboy141@gmail.com>
2025-12-02 12:08:43 +0530

    loop pratic

---
+++ looppratic.py
@@ -0,0 +1,28 @@
+# a=int(input("number"))
+# b=1
+# while b<=10:
+#     print(a*b)
+#     print(b)
+#     b+=1
+
+# a=int(input("Number 1:"))
+# b=int(input("Number 2:"))
+# c=int(input("Number 3:"))
+# d=int(input("Number 4:"))
+# e=int(input("Number 5:"))
+# f=int(input("Number 6:"))
+# g=int(input("Number 7:"))
+# h=int(input("Number 8:"))
+# i=int(input("Number 9:"))
+# j=int(input("Number 10:"))
+
+
+# print(a+b+c+d+e+f+g+h+i+j)
+    
+# add=0
+# i=1
+# while i<=10:
+#  c=int(input("number"))
+#  add=add+c
+#  i+=1
+#  print(add)
