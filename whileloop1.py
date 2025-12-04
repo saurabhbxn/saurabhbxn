099f89f73fa6d5d05f558ffff1181e0181b4c788
saurabhbxn <bayanaboy141@gmail.com>
2025-12-04 12:44:12 +0530

    while loop pratic

---
+++ whileloop1.py
@@ -0,0 +1,56 @@
+# sum = 0
+# while True:
+#     number = int(input("Enter a number : "))
+#     if number < 0:
+#         break
+#     else:
+#         sum = sum + number
+#     print(sum)
+
+# print(sum)        
+
+# a = "i am from bharatpur and i live in bayana"
+# print("bayana" in a)
+
+# a = 23
+# b = [1,12,13,14,16,198,10,"231"]
+
+# print( 12  in b)
+
+
+# a = input("Enter a character")
+
+# if a == "a" or a=="e" or a== "i" or a== "o" or a== "u":
+#    print("its a vowel")
+# else:
+#    print("its a not vowel")   
+
+# my_intro = """my name is saurabh
+# i am from bharatpur
+# i am live in bayana"""
+
+# print(my_intro)
+
+# str1 = "hello"
+# str2 = "world"
+# result = str1 + " " + str2
+# print(result) 
+
+# my_string = "hello"
+# print(my_string[4])
+
+# a = "sjdghgjsdhgjhsgfjh"
+# print(len(a))
+
+
+# a = "i am from bayana"
+
+# b = len(a)
+
+# k = 0
+# while k < b:
+#   print(a)
+#   k += 1    
+
+# a = "i am saurabh"
+# print(len(a))
