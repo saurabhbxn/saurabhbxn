9fec1614673502af0997452d277873f06ac40a74
saurabhbxn <bayanaboy141@gmail.com>
2025-12-11 10:43:07 +0530

    loop bxn

---
+++ loopbxn.py
@@ -0,0 +1,41 @@
+# a = "vipin"
+# l = len(a)
+# i = 0
+# while i<l:
+#     print(a[i])
+#     i+=1
+
+# a =input("Enter your data")
+# c = 0
+# i = 0
+# while i< len(a):
+#     if a[i] == "i":
+#        c=c+1
+
+#     i +=1
+# print(c)    
+
+
+# a = input("Enter your date")
+# c = 0
+# b = 0
+# while b< len(a):
+#     if a[b]=="a" or a[b]=="e" or a[b]=="i" or a[b]=="o" or a[b]=="u":
+    
+#         c=c+1
+#     b+=1
+# print(c)
+# 
+
+
+# membership 
+# a = input("Enter your date")
+# c = 0
+# b = 0
+# while b< len(a):
+#     if a[b] in "aeiou":
+    
+#         c=c+1
+#     b+=1
+# print(c)                
+
