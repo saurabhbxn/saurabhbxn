a8450e9a925fac304c5b24e9ca058842c0805373
saurabhbxn <bayanaboy141@gmail.com>
2026-01-07 21:00:24 +0530

    Add files via upload
    7 /01/26 loop

---
+++ looptask1.py
@@ -0,0 +1,32 @@
+# a = 1
+# while a < 10:
+#     print(a)
+#     a +=1
+
+
+# bxn = 1
+# while bxn <=20:
+#     print(bxn)
+#     bxn +=1
+
+# bxn = 1
+# while bxn <=100:
+#     bxn +=1
+#     print(bxn)
+    
+
+# cd = 10
+# while cd >=1:
+#  cd -=1
+#  print(cd)
+
+# cd = 50
+# while cd >=0:
+#  cd -=2
+#  print(cd)
+
+
+# cd = 50
+# while cd >=0:
+#  cd -=1
+#  print(cd)
