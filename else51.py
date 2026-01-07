5b7290e6cb832a11c2e41d082ddc069b18978fe4
saurabhbxn <bayanaboy141@gmail.com>
2026-01-07 21:04:08 +0530

    if and else pratic

---
+++ else51.py
@@ -0,0 +1,25 @@
+
+
+number = int(input("Enter A : number"))
+
+if number == 1:
+    print("monday")
+elif number == 2:
+    print("tuesday")
+elif number == 3:
+    print("wednesday") 
+elif number == 4:
+    print("thrusday")    
+
+elif number == 5:
+    print("friday")  
+
+elif number == 6:
+    print("saturday") 
+
+elif number == 7:
+    print("sunday")
+
+else :
+    print("invalid number")       
+
