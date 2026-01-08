d63cdaec19ddbe7c6c44418c80535f47ee1f2c24
saurabhbxn <bayanaboy141@gmail.com>
2026-01-08 13:38:35 +0530

    loop table

---
+++ looptabletask.py
@@ -0,0 +1,34 @@
+# number = int(input("Enter A number: "))
+# a = 5
+# while a <= number * 10:
+#     print(a)
+#     a +=number
+
+
+
+# number = int(input("Enter A number: "))
+# a = 255
+# while a <= number * 10:
+#     print(a)
+#     a +=number
+
+
+
+
+# number = int(input("Enter A number: "))
+# a = number
+# while a <= number * 10:
+#     print(a)
+#     a +=number
+
+# number = int(input("Enter A number: "))
+# a = 1
+# while a<=number:
+#     if a % 2==0
+#     print(a)
+
+a = 1
+while a <= 10:
+    if a % 2 != 0 :
+     print(a)
+    a +=1
