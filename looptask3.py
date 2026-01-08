305aece25ee8408a8bf7648f46c4a8abb0872825
saurabhbxn <bayanaboy141@gmail.com>
2026-01-08 13:39:28 +0530

    loop plus minus

---
+++ looptask3.py
@@ -0,0 +1,11 @@
+# number = int(input("Enter A number: "))
+# n = 100
+# while n>=number:
+#     print(-n)
+#     n -=1
+
+number = int(input("Enter A number: "))
+n = 1
+while n<=number:
+    print(-n)
+    n +=1
