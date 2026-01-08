6d5374259c615a243e86f31678a3769341802e6e
saurabhbxn <bayanaboy141@gmail.com>
2026-01-08 13:40:15 +0530

    loop mix

---
+++ looptask2.py
@@ -0,0 +1,48 @@
+# number = int(input("Enter A number: "))
+# counter  = 0
+# while counter <number:
+#     counter +=1
+#     print(counter)
+
+
+# number = int(input("Enter A number: "))
+# counter = 1
+# while counter<=number:
+#     counter +=1
+#     print(counter)
+
+
+# while True :
+#     a = input("Enter a number : ")
+#     print(a)
+
+# counter = 1
+# while counter <=10:
+#     if counter % 2 == 0:
+#         print (counter)
+#         counter +=1
+
+
+# number = int(input("Enter a number: "))
+# counter = 100
+# while counter  >=   number:
+#     print(counter)
+#     counter   -=1
+
+# number = int(input("Enter a number: "))
+# counter = number
+# while counter <= number * 10:
+#     print(counter)
+#     counter +=number
+
+# number = int(input("Enter A number: "))
+# n = 10
+# while n<=number:
+#     print(-n)
+#     n +=1
+
+number = int(input("Enter A number: "))
+n = 100
+while n>=number:
+    print(-n)
+    n -=1
