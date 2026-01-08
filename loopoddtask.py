5fb9f85b8d97fdfb41f5f8b5a37c1b16b1abcb30
saurabhbxn <bayanaboy141@gmail.com>
2026-01-08 13:38:00 +0530

    loop od number

---
+++ loopoddtask.py
@@ -0,0 +1,17 @@
+# cd = 1
+# while cd <=56:
+#     if cd % 2 != 0:
+#      print(cd)
+#     cd +=1 
+
+# cd = 1
+# while cd <=200:
+#     if cd % 2 != 0:
+#      print(cd)
+#     cd +=1 
+
+# ds = 1
+# while ds<=150:
+#     if ds % 2 == 0:
+#         print(ds)
+#     ds +=1    
